ac2dc18ec67407e0d05d1111997cac5672cfe3af
samuelBarreto <samukacfc1@gmail.com>
2025-07-10 10:39:57 -0300

    run: terraform fmt -check

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,17 +16,17 @@ provider "aws" {
 # Módulo VPC
 module "vpc" {
   source = "./modules/vpc"
-  
-  vpc_cidr             = var.vpc_cidr
-  environment          = var.environment
-  availability_zones   = var.availability_zones
-  public_subnet_cidrs  = var.public_subnet_cidrs
+
+  vpc_cidr            = var.vpc_cidr
+  environment         = var.environment
+  availability_zones  = var.availability_zones
+  public_subnet_cidrs = var.public_subnet_cidrs
 }
 
 # Módulo Security Groups
 module "security_groups" {
   source = "./modules/security_groups"
-  
+
   vpc_id      = module.vpc.vpc_id
   environment = var.environment
 }
@@ -34,14 +34,14 @@ module "security_groups" {
 # Módulo EC2
 module "ec2" {
   source = "./modules/ec2"
-  
-  environment           = var.environment
-  instance_type         = var.instance_type
-  key_name              = var.key_name
-  vpc_id                = module.vpc.vpc_id
-  public_subnet_id      = module.vpc.public_subnet_ids[0]
-  security_group_ids    = [module.security_groups.web_sg_id]
-  user_data_template    = file("${path.module}/templates/user_data.sh")
-  create_key_pair       = var.create_key_pair
-  public_key            = var.public_key
+
+  environment        = var.environment
+  instance_type      = var.instance_type
+  key_name           = var.key_name
+  vpc_id             = module.vpc.vpc_id
+  public_subnet_id   = module.vpc.public_subnet_ids[0]
+  security_group_ids = [module.security_groups.web_sg_id]
+  user_data_template = file("${path.module}/templates/user_data.sh")
+  create_key_pair    = var.create_key_pair
+  public_key         = var.public_key
 } 
